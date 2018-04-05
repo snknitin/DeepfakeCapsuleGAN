26ba2cd4f53806ef5461ae2425420d8b524fa3d9
Nitin Kishore Sai Samala <snk.nitin@gmail.com>
2018-04-04 23:38:29 -0400

    Basic stuff

---
+++ Code/model.py
@@ -0,0 +1,24 @@
+import os
+from time import time
+import pandas as pd
+
+class Solution(object):
+    def __init__(self):
+
+        self.X_train,self.X_test,self.y_train,self.y_test,self.dataframe_all=None,None,None,None
+
+    def visualize(self):
+        pass
+
+    def train_generator(self):
+    	pass
+
+    def train_discriminator(self):
+        pass
+
+    
+
+if __name__=="__main__":
+    s=Solution()
+
+
