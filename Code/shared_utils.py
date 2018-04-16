2d43761be5a362670228a3d8e425e25e0ee224eb
Nitin Kishore Sai Samala <snk.nitin@gmail.com>
2018-04-15 21:33:12 -0400

    Utilities to save images

--- Code/shared_utils.py
+++ Code/shared_utils.py
@@ -1,7 +1,12 @@
 import os
-import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+from keras import backend as K
+import skimage
+from skimage import data, color, exposure
+from skimage.transform import resize
+import matplotlib.pyplot as plt
+
 
 IMAGES_PATH=os.path.join(os.getcwd(),"static/")
 if not os.path.exists(IMAGES_PATH):
@@ -13,6 +18,48 @@ def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
     if tight_layout:
         plt.tight_layout()
     plt.savefig(path, format=fig_extension, dpi=resolution)
+
+def save_imgs(dataset_title,generator, epoch):
+    r, c = 5, 5
+    noise = np.random.normal(0, 1, (r * c, 100))
+    gen_imgs = generator.predict(noise)
+
+    # rescale images 0 - 1
+    gen_imgs = 0.5 * gen_imgs + 0.5
+
+    fig, axs = plt.subplots(r, c)
+    cnt = 0
+
+    # iterate in order to create a subplot
+    for i in range(r):
+        for j in range(c):
+            if dataset_title == 'mnist':
+                axs[i, j].imshow(gen_imgs[cnt, :, :, 0], cmap='gray')
+                axs[i, j].axis('off')
+                cnt += 1
+            elif dataset_title == 'cifar10':
+                axs[i, j].imshow(gen_imgs[cnt, :, :, :])
+                axs[i, j].axis('off')
+                cnt += 1
+            else:
+                print('Please indicate the image options.')
+
+
+    path = os.path.join(IMAGES_PATH, 'images_{0}'.format(dataset_title))
+    fig.savefig(path)
+    plt.close()
+
+# squash function of capsule layers, borrowed from Xifeng Guo's implementation of Keras CapsNet `https://github.com/XifengGuo/CapsNet-Keras`
+def squash(vectors, axis=-1):
+    """
+    The non-linear activation used in Capsule. It drives the length of a large vector to near 1 and small vector to 0
+    :param vectors: some vectors to be squashed, N-dim tensor
+    :param axis: the axis to squash
+    :return: a Tensor with same shape as input vectors
+    """
+    s_squared_norm = K.sum(K.square(vectors), axis, keepdims=True)
+    scale = s_squared_norm / (1 + s_squared_norm) / K.sqrt(s_squared_norm + K.epsilon())
+    return scale * vectors
 
 
 def load():
