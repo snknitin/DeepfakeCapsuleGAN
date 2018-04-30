c72f2b059e70db586b215285340ec2e1c1f32419
Nitin Kishore Sai Samala <snk.nitin@gmail.com>
2018-04-30 18:26:49 -0400

    remove other data

--- Code/create_img.py
+++ Code/create_img.py
@@ -14,10 +14,10 @@ import shared_utils as su
 
 if __name__=="__main__":
     dataset_title="cifar10"
-    mnist_loc= "E:\\GIT_ROOT\\DeepfakeCapsuleGAN\\Data\\log\\mnist_train_02\\mnist\\saved_models\\mnist_gen_model_46000.h5"
+    #mnist_loc= "E:\\GIT_ROOT\\DeepfakeCapsuleGAN\\Data\\log\\mnist_train_02\\mnist\\saved_models\\mnist_gen_model_46000.h5"
     #cifar_loc= "E:\\GIT_ROOT\\DeepfakeCapsuleGAN\\Data\\log\\cifar_train_01\\cifar10\\saved_models\\cifar10_gen_model_132000.h5"
     cifar_loc="/mnt/nfs/scratch1/nsamala/capsuleGAN/DeepfakeCapsuleGAN/Data/log/cifar_train_01/cifar10/saved_models/cifar10_gen_model_132000.h5"
-    celeba_loc = "E:\\GIT_ROOT\\DeepfakeCapsuleGAN\\Data\\log\\celeba_train_01\\celeba\\saved_models\\celeba_gen_model_128000.h5"
+    #celeba_loc = "E:\\GIT_ROOT\\DeepfakeCapsuleGAN\\Data\\log\\celeba_train_01\\celeba\\saved_models\\celeba_gen_model_128000.h5"
 
     generator=load_model(cifar_loc)
     su.save_gen_img(dataset_title,generator,100)
